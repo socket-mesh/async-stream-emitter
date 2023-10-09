830f037d58356696b1b688dd8168ab01c6f1d21c
Greg Kimmy <greg.kimmy@gmail.com>
2023-10-09 16:58:37 -0500

    chore: spacing

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,59 +4,59 @@ import { beforeEach, afterEach, describe, it, expect } from '@jest/globals';
 let pendingTimeoutSet = new Set<NodeJS.Timeout>();
 
 function cancelAllPendingWaits() {
-  for (let timeout of pendingTimeoutSet) {
-    clearTimeout(timeout);
-  }
+	for (let timeout of pendingTimeoutSet) {
+		clearTimeout(timeout);
+	}
 }
 
 describe('AsyncStreamEmitter', () => {
-  let streamEmitter: AsyncStreamEmitter<string>;
+	let streamEmitter: AsyncStreamEmitter<string>;
 
-  beforeEach(async () => {
-    streamEmitter = new AsyncStreamEmitter<string>();
-  });
+	beforeEach(async () => {
+		streamEmitter = new AsyncStreamEmitter<string>();
+	});
 
-  afterEach(async () => {
-    cancelAllPendingWaits();
-  });
+	afterEach(async () => {
+		cancelAllPendingWaits();
+	});
 
-  it('should expose an emit method', () => {
-    expect(!!streamEmitter.emit).toBe(true);
-  });
+	it('should expose an emit method', () => {
+		expect(!!streamEmitter.emit).toBe(true);
+	});
 
-  it('should expose a listen method', () => {
-    expect(!!streamEmitter.listen).toBe(true);
-  });
+	it('should expose a listen method', () => {
+		expect(!!streamEmitter.listen).toBe(true);
+	});
 
-  it('should expose a closeListener method', () => {
-    expect(!!streamEmitter.closeListeners).toBe(true);
-  });
+	it('should expose a closeListener method', () => {
+		expect(!!streamEmitter.closeListeners).toBe(true);
+	});
 
-  it('should expose a closeAllListeners method', () => {
-    expect(!!streamEmitter.closeListeners).toBe(true);
-  });
+	it('should expose a closeAllListeners method', () => {
+		expect(!!streamEmitter.closeListeners).toBe(true);
+	});
 
-  it('should expose a getListenerConsumerStats method', () => {
-    expect(!!streamEmitter.getListenerConsumerStats).toBe(true);
-  });
+	it('should expose a getListenerConsumerStats method', () => {
+		expect(!!streamEmitter.getListenerConsumerStats).toBe(true);
+	});
 
-  it('should expose a killListener method', () => {
-    expect(!!streamEmitter.killListeners).toBe(true);
-  });
+	it('should expose a killListener method', () => {
+		expect(!!streamEmitter.killListeners).toBe(true);
+	});
 
-  it('should expose a killAllListeners method', () => {
-    expect(!!streamEmitter.killListeners).toBe(true);
-  });
+	it('should expose a killAllListeners method', () => {
+		expect(!!streamEmitter.killListeners).toBe(true);
+	});
 
-  it('should expose a killListenerConsumer method', () => {
-    expect(!!streamEmitter.killListeners).toBe(true);
-  });
+	it('should expose a killListenerConsumer method', () => {
+		expect(!!streamEmitter.killListeners).toBe(true);
+	});
 
-  it('should expose a getListenerBackpressure method', () => {
-    expect(!!streamEmitter.getListenerBackpressure).toBe(true);
-  });
+	it('should expose a getListenerBackpressure method', () => {
+		expect(!!streamEmitter.getListenerBackpressure).toBe(true);
+	});
 
-  it('should expose a hasListenerConsumer method', () => {
-    expect(!!streamEmitter.hasListenerConsumer).toBe(true);
-  });
+	it('should expose a hasListenerConsumer method', () => {
+		expect(!!streamEmitter.hasListenerConsumer).toBe(true);
+	});
 });
